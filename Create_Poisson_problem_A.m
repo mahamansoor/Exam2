86cc1badf88d8ccd9da79315d4efe2bf49a0d36e
xoxo <113355280+mahamansoor@users.noreply.github.com>
2023-03-27 03:06:23 +0000

    Update Create_Poisson_problem_A.m

--- Create_Poisson_problem_A.m
+++ Create_Poisson_problem_A.m
@@ -7,24 +7,24 @@ function [ A ] = Create_Poisson_problem_A( N )
 %matlab code, the resulting matrix A is symmetric and positive definite. 
 % N: size of the N x N mesh (meaning there are N^2 interior points)
 % h: distance between grid points
-h = 1 / (N+1);
-% The above avriable will creates a (N^2) x (N^2) matrix of zeros
+h = 1 / (N+1); % we will set the distance between the grid points 
+% The above variable will creates a (N^2) x (N^2) matrix of zeros
 A = zeros(N^2);
 % Populate the matrix with the stencil values and a matrix of zeros with
 % dimensions (N^2) x N^2 is created. 
-for i=1:N^2
+for i=1:N^2 % in this step we will apply the for loop 
     % we populate this matrix by stensil values for the poisson equation
     % with the help of nested loops. 
-A(i,i) = 4;
+A(i,i) = 4; % here we will set the matrix A values 
 % Check if the current point is on the left or right boundary
-if mod(i-1,N) == 0
+if mod(i-1,N) == 0 % we will then if mod (i-1, N) is equal to 0
 % The point is on the left boundary
 % Only include the stencil values to the right, up, and down of the
 % current point (don't include the value to the left)
-if i > N
-A(i,i-N) = -1;
+if i > N % the if statement is used to check if i > N
+A(i,i-N) = -1; % this equation will initialize the A(i, i-N) equal to -1 
 end
-if i <= N*(N-1)
+if i <= N*(N-1) % another if statement is applied to set the matrix diagonal entries 
 A(i,i+N) = -1;
 end
 A(i,i+1) = -1;
@@ -49,37 +49,29 @@ A(i,i+N) = -1;
 end
 end
 % Scale the matrix by h^2
-A = (1 / h^2) * A;
+A = (1 / h^2) * A; %h^2 values are used to scale the matrix in order to create the Poisson matrix A 
 end
-end
-
+end % This code creates a matrix with size N with boundary points, size of grid, stencil values to creste poisson matrix A 
 % can also be done by the second code 
 function A = Create_Poisson_problem_A(N)
-% Creates the matrix A for the Poisson problem on a unit square.
-% Input:
+% we can create the matrix A for the Poisson problem on a unit square.
+% we can use following variables for our input 
 %   N: The size of the N x N mesh (meaning there are N^2 interior points).
-% Output:
-%   A: The Poisson matrix.
-
-% Compute the grid spacing.
+% we can then compute the grid spacing.
 h = 1/(N+1);
-
-% Construct the diagonal entries.
+% we will then construct the diagonal entries.
 diag_entries = -4*ones(N*N,1);
-
-% Construct the off-diagonal entries for the x direction.
+% we then construct the off-diagonal entries for the x direction.
 x_entries = ones(N*N,1);
 for i = 2:N
     x_entries((i-1)*N+1:i*N) = 1-h^2;
 end
-
 % Construct the off-diagonal entries for the y direction.
 y_entries = ones(N*N,1);
 for i = 1:N-1
     y_entries(i*N+1:(i+1)*N) = 1-h^2;
 end
-
 % Assemble the matrix A.
 A = spdiags([diag_entries, y_entries, x_entries, y_entries, x_entries],...
             [-N,-1,0,1,N], N*N, N*N);
-end
+end % finally we will print the poisson matrix A(N)
